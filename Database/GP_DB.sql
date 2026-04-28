e032d0a41b7beced1c41126724550b6808eb0368
Abood Abualrob <abdalkhalqabualrob@gmail.com>
2026-04-28 15:48:48 +0300

    adding database in separate file

---
+++ Database/GP_DB.sql
@@ -0,0 +1,363 @@
+CREATE DATABASE  IF NOT EXISTS `vetcommunity` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `vetcommunity`;
+-- MySQL dump 10.13  Distrib 8.0.44, for Win64 (x86_64)
+--
+-- Host: localhost    Database: vetcommunity
+-- ------------------------------------------------------
+-- Server version	8.0.44
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `adoption_posts`
+--
+
+DROP TABLE IF EXISTS `adoption_posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `adoption_posts` (
+  `adoption_status` enum('CANCELLED','COMPLETED','OPEN','RESERVED') NOT NULL,
+  `city` varchar(255) NOT NULL,
+  `id` bigint NOT NULL,
+  `pet_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKrpmnbqb0vuug28u6xlif7u2om` (`pet_id`),
+  CONSTRAINT `FK7wk2il54tfn9rd92fdf4iw6m6` FOREIGN KEY (`id`) REFERENCES `posts` (`id`),
+  CONSTRAINT `FKrpmnbqb0vuug28u6xlif7u2om` FOREIGN KEY (`pet_id`) REFERENCES `pet` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `adoption_posts`
+--
+
+LOCK TABLES `adoption_posts` WRITE;
+/*!40000 ALTER TABLE `adoption_posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `adoption_posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `adoption_requests`
+--
+
+DROP TABLE IF EXISTS `adoption_requests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `adoption_requests` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `city` varchar(80) NOT NULL,
+  `created_at` datetime(6) DEFAULT NULL,
+  `message` varchar(500) DEFAULT NULL,
+  `phone_number` varchar(30) NOT NULL,
+  `status` enum('ACCEPTED','PENDING','REJECTED') NOT NULL,
+  `adoption_post_id` bigint NOT NULL,
+  `requester_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKqpudsue6wdwgvmdjls2cjo8qn` (`adoption_post_id`),
+  KEY `FKbaghuhfvg0wi49ouvhjqmhbea` (`requester_id`),
+  CONSTRAINT `FKbaghuhfvg0wi49ouvhjqmhbea` FOREIGN KEY (`requester_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `FKqpudsue6wdwgvmdjls2cjo8qn` FOREIGN KEY (`adoption_post_id`) REFERENCES `adoption_posts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `adoption_requests`
+--
+
+LOCK TABLES `adoption_requests` WRITE;
+/*!40000 ALTER TABLE `adoption_requests` DISABLE KEYS */;
+/*!40000 ALTER TABLE `adoption_requests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clinic`
+--
+
+DROP TABLE IF EXISTS `clinic`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clinic` (
+  `user_id` bigint NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `FK170twqkl3qdy9u4ind0fdo3bb` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clinic`
+--
+
+LOCK TABLES `clinic` WRITE;
+/*!40000 ALTER TABLE `clinic` DISABLE KEYS */;
+INSERT INTO `clinic` VALUES (1,'Irbid','Irbid',32.54746433163718,35.85697174072266),(2,'Irbid','Irbid',32.48637963013472,35.99215507507325),(3,'Irbid','Irbid',32.46918302793641,36.05828762054444);
+/*!40000 ALTER TABLE `clinic` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comments` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `content` varchar(1000) DEFAULT NULL,
+  `created_at` datetime(6) DEFAULT NULL,
+  `post_id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKh4c7lvsc298whoyd4w9ta25cr` (`post_id`),
+  KEY `FK8omq0tc18jd43bu5tjh6jvraq` (`user_id`),
+  CONSTRAINT `FK8omq0tc18jd43bu5tjh6jvraq` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `FKh4c7lvsc298whoyd4w9ta25cr` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `comments`
+--
+
+LOCK TABLES `comments` WRITE;
+/*!40000 ALTER TABLE `comments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pet`
+--
+
+DROP TABLE IF EXISTS `pet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pet` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `age_years` double DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `has_vaccine_cert` bit(1) DEFAULT NULL,
+  `health_status` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `photo_url` varchar(255) DEFAULT NULL,
+  `species` varchar(255) DEFAULT NULL,
+  `owner_id` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK18ujlvblqmcku8ydgo5wvvgmo` (`owner_id`),
+  CONSTRAINT `FK18ujlvblqmcku8ydgo5wvvgmo` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pet`
+--
+
+LOCK TABLES `pet` WRITE;
+/*!40000 ALTER TABLE `pet` DISABLE KEYS */;
+/*!40000 ALTER TABLE `pet` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pet_owner`
+--
+
+DROP TABLE IF EXISTS `pet_owner`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pet_owner` (
+  `user_id` bigint NOT NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `FKjyq8cp78qldq2bi2csmoo4uh3` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pet_owner`
+--
+
+LOCK TABLES `pet_owner` WRITE;
+/*!40000 ALTER TABLE `pet_owner` DISABLE KEYS */;
+INSERT INTO `pet_owner` VALUES (4);
+/*!40000 ALTER TABLE `pet_owner` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `posts`
+--
+
+DROP TABLE IF EXISTS `posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `post_type` varchar(31) NOT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `content` text,
+  `created_at` datetime(6) DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `owner_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK452mmghnrxq6h7f4f5v8ge95f` (`owner_id`),
+  CONSTRAINT `FK452mmghnrxq6h7f4f5v8ge95f` FOREIGN KEY (`owner_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `posts_chk_1` CHECK ((`post_type` in (_utf8mb4'ADOPTION',_utf8mb4'REGULAR')))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posts`
+--
+
+LOCK TABLES `posts` WRITE;
+/*!40000 ALTER TABLE `posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `regular_posts`
+--
+
+DROP TABLE IF EXISTS `regular_posts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `regular_posts` (
+  `id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `FK4im3t8h4ndxar6fc5oej1sk38` FOREIGN KEY (`id`) REFERENCES `posts` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `regular_posts`
+--
+
+LOCK TABLES `regular_posts` WRITE;
+/*!40000 ALTER TABLE `regular_posts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `regular_posts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `service`
+--
+
+DROP TABLE IF EXISTS `service`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `service` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `description` text,
+  `name` varchar(255) DEFAULT NULL,
+  `user_id` bigint NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FKpbq9opvw9yixivp9kc9qqwclv` (`user_id`),
+  CONSTRAINT `FKpbq9opvw9yixivp9kc9qqwclv` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `service`
+--
+
+LOCK TABLES `service` WRITE;
+/*!40000 ALTER TABLE `service` DISABLE KEYS */;
+/*!40000 ALTER TABLE `service` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_profile`
+--
+
+DROP TABLE IF EXISTS `user_profile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_profile` (
+  `user_id` bigint NOT NULL,
+  `bio` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `photo_url` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `FKuganfwvnbll4kn2a3jeyxtyi` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_profile`
+--
+
+LOCK TABLES `user_profile` WRITE;
+/*!40000 ALTER TABLE `user_profile` DISABLE KEYS */;
+INSERT INTO `user_profile` VALUES (1,'','clinic','test','https://res.cloudinary.com/di1xpud7d/image/upload/v1767881554/clinic_d2jav0.jpg'),(2,'','clinicOne','test','https://res.cloudinary.com/di1xpud7d/image/upload/v1767881554/clinic_d2jav0.jpg'),(3,'','clinicTwo','test','https://res.cloudinary.com/di1xpud7d/image/upload/v1767881554/clinic_d2jav0.jpg'),(4,'','owner','test','https://res.cloudinary.com/di1xpud7d/image/upload/v1767881569/owner_qazol0.jpg');
+/*!40000 ALTER TABLE `user_profile` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `password_hash` varchar(255) DEFAULT NULL,
+  `role` enum('ADMIN','CLINIC','OWNER','VET') DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK6dotkott2kjsp8vw4d0m25fb7` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'clinic@gmail.com','$2a$10$OAFoL6B7zjVHxkp9aDn7BOmgT2k5DqkN6mmB2/d/M6iPD6gcZWUG2','CLINIC'),(2,'clinic1@gmail.com','$2a$10$a.E9.FiFQEyQTYu6HCVQ4u30l6ng0LuV70OVO8RTZDEcB73XA/u1q','CLINIC'),(3,'clinic2@gmail.com','$2a$10$qXLYfj0IuXKLrlEhA0FGL.BfLyQ9jZtwBQmkfTv1UazEBqjtENfR6','CLINIC'),(4,'owner@gmail.com','$2a$10$G5/OsS56rfj/7rDph6pWYeTCrIZKg/4aZKhFNnD2yQslAUoevraXO','OWNER');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vet`
+--
+
+DROP TABLE IF EXISTS `vet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `vet` (
+  `user_id` bigint NOT NULL,
+  `specialty` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `FKduyxyrxl9xm4c2137gy7v0vok` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vet`
+--
+
+LOCK TABLES `vet` WRITE;
+/*!40000 ALTER TABLE `vet` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vet` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-04-28 15:44:17
